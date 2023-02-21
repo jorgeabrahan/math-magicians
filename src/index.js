0b88a0e12bb756b33ede56caef7fbb4512fcaa4d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-21 15:44:50 -0600

    Wrap app in BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
