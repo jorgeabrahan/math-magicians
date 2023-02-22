1ba027bd7c20cd140e299bcd66fe969adbbe18d4
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-22 09:15:15 -0600

    Create snapshot test for Quote component

--- src/tests/Snapshots.test.js
+++ src/tests/Snapshots.test.js
@@ -2,6 +2,7 @@
 import renderer from 'react-test-renderer';
 import Calculator from '../routes/Calculator/index';
 import Home from '../routes/Home';
+import Quote from '../routes/Quote';
 
 describe('Snapshot tests for all components', () => {
   it('Should render the calculator component correctly', () => {
@@ -9,5 +10,8 @@ describe('Snapshot tests for all components', () => {
   });
   it('Should render the Home component correctly', () => {
     expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
+  });
+  it('Should render the Quote component correctly', () => {
+    expect(renderer.create(<Quote />).toJSON()).toMatchSnapshot();
   });
 });
