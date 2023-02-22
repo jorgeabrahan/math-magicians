b3c9b9af9b4236de30a896e82637bcdef8471ed4
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-22 09:18:44 -0600

    Create snapshot test for Number and Symbol components

--- src/tests/Snapshots.test.js
+++ src/tests/Snapshots.test.js
@@ -1,12 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import renderer from 'react-test-renderer';
 import Calculator from '../routes/Calculator/index';
+import Number from '../routes/Calculator/Number';
+import Symbol from '../routes/Calculator/Symbol';
 import Home from '../routes/Home';
 import Quote from '../routes/Quote';
 
 describe('Snapshot tests for all components', () => {
-  it('Should render the calculator component correctly', () => {
+  it('Should render the Calculator component correctly', () => {
     expect(renderer.create(<Calculator />).toJSON()).toMatchSnapshot();
+  });
+  it('Should render the Number component correctly', () => {
+    expect(renderer.create(<Number />).toJSON()).toMatchSnapshot();
+  });
+  it('Should render the Symbol component correctly', () => {
+    expect(renderer.create(<Symbol />).toJSON()).toMatchSnapshot();
   });
   it('Should render the Home component correctly', () => {
     expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
