48ede0db587d0bd17359e7b971d95d1e737ef284
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-22 09:13:20 -0600

    Create snapshot test for Home component

--- src/tests/Snapshots.test.js
+++ src/tests/Snapshots.test.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import renderer from 'react-test-renderer';
 import Calculator from '../routes/Calculator/index';
+import Home from '../routes/Home';
 
 describe('Snapshot tests for all components', () => {
   it('Should render the calculator component correctly', () => {
-    const tree = renderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(renderer.create(<Calculator />).toJSON()).toMatchSnapshot();
+  });
+  it('Should render the Home component correctly', () => {
+    expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
   });
 });
