766703a8c9742aa810932b79adfbe4ae08498e25
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-22 12:12:54 -0700

    Fix linters

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,32 +1,30 @@
-import operate from '../routes/Calculator/logic/operate.js';
+import operate from '../routes/Calculator/logic/operate';
 
 /**
  * @jest-environment jsdom
  */
 
 describe('Testing operate.js', () => {
+  test('Test + - ÷ x ', () => {
+    const numberOne = '7';
+    const numberTwo = '3';
+    expect(operate(numberOne, numberTwo, '+')).toBe('10');
+    expect(operate(numberOne, numberTwo, '-')).toBe('4');
 
-    test('Test + - ÷ x ', () => {
-        const numberOne = '7';
-        const numberTwo = '3';
-        expect(operate(numberOne, numberTwo, '+')).toBe('10');
-        expect(operate(numberOne, numberTwo, '-')).toBe('4');
+    let num = parseFloat(operate(numberOne, numberTwo, '÷'));
+    expect(num).toBeCloseTo(2.333333, 6);
 
-        let num = parseFloat(operate(numberOne, numberTwo, '÷') );
-        expect( num  ).toBeCloseTo(2.333333,6);
+    num = parseFloat(operate(numberOne, numberTwo, 'x'));
+    expect(num).toBe(21);
 
-        num = parseFloat(operate(numberOne, numberTwo, 'x') );
-        expect( num  ).toBe(21);
+    num = parseFloat(operate(numberOne, numberTwo, '%'));
+    expect(num).toBe(1);
+  });
 
-        num = parseFloat(operate(numberOne, numberTwo, '%') );
-        expect( num ).toBe(1);
-
-    });
-
-    test('Special cases', () => {
-        const numberOne = '7';
-        const numberTwo = '0';
-        expect( parseFloat(operate(numberOne, numberTwo, '÷') )  ).toBe(NaN);
-        expect( parseFloat(operate(numberOne, numberTwo, '%') )  ).toBe(NaN);
-    })
+  test('Special cases', () => {
+    const numberOne = '7';
+    const numberTwo = '0';
+    expect(parseFloat(operate(numberOne, numberTwo, '÷'))).toBe(NaN);
+    expect(parseFloat(operate(numberOne, numberTwo, '%'))).toBe(NaN);
+  });
 });
