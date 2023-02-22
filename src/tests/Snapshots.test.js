0cab84ce59b793347b4cd3e287194f5c2054fbc6
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-22 09:11:37 -0600

    Create snapshot test for Calculator component

--- src/tests/Snapshots.test.js
+++ src/tests/Snapshots.test.js
@@ -1,1 +1,10 @@
-describe('Snapshot tests for all components', () => {});
+// eslint-disable-next-line import/no-extraneous-dependencies
+import renderer from 'react-test-renderer';
+import Calculator from '../routes/Calculator/index';
+
+describe('Snapshot tests for all components', () => {
+  it('Should render the calculator component correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
